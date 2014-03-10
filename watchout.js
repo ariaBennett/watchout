56a245c016e1639f7622e584bef47e11d56fa273
Hack Reactor Students <twoplustwo@gmail.com>
2014-03-10 13:19:39 -0700

    pairity with my pair

--- watchout.js
+++ watchout.js
@@ -1,74 +1,323 @@
-// start slingin' some d3 here.
+/*
+window.wo={};
 
-
-// var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
-// var width = 960,
-//     height = 500;
-var gameOptions = {
-  height: 400,
-  width: 700,
-  nEnemies: 30,
-  padding: 20
+wo.options = {
+  height: window.innerHeight,
+  width: window.innerWidth
 };
 
-var gameStats ={
-  score: 0,
-  bestScore: 0
+wo.gameBoard = d3.select('body').append('svg:svg').attr('width', wo.options.width).attr('height', wo.options.height);
+
+wo.handleResize = function(){
+  window.onresize = function(){
+    wo.options.height = window.innerHeight;
+    wo.options.width = window.innerWidth;
+    wo.gameBoard.attr("width", window.innerHeight);
+    wo.gameBoard.attr("width", window.innerWidth);
+
+  };
 };
 
-var axes ={
-  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
-  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+wo.createLevel = function(){
+  wo.gameBoard.append('circle')
+  .attr('cx', '50%').attr('cy', '50%').attr('r', '5%')
+  .attr('stroke', 'black').attr('stroke-width', '0.5%')
+  .attr('fill', 'green');
+};
+
+// Run Section
+wo.handleResize();
+wo.createLevel();
+*/
+
+
+// copy paste below
+
+
+
+var diameter = 960;
+
+var tree = d3.layout.tree()
+    .size([360, diameter / 2 - 240])
+    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+var diagonal = d3.svg.diagonal.radial()
+    .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", diameter - 350)
+    .attr("height", diameter - 150)
+  .append("g")
+    .attr("transform", "translate(" + diameter / 3 + "," + diameter / 3 + ")");
+
+
+var jsonThing = {
+  "name": "Marcus",
+  "children": [
+    {
+      "name": "floor 1",
+      "children": [
+        {
+          "name": "floor 1 help 1",
+          "children": [
+            {"name": "WeiMao", "size": 3938},
+            {"name": "TristanYu", "size": 3812},
+            {"name": "SeanSun", "size": 743}
+          ]
+        },
+        {
+          "name": "floor 1 help 2",
+          "children": [
+            {"name": "RobertHolmes", "size": 3534},
+            {"name": "PhillipAlexander", "size": 5731},
+            {"name": "KatieHempenius", "size": 5914}
+          ]
+        },
+        {
+          "name": "floor 1 help 3",
+          "children": [
+            {"name": "JoshuaGoldberg"},
+            {"name": "JonathanTewksbury"},
+            {"name": "FaridSiddiqi"}
+          ]
+        }
+      ]
+    },
+    {
+      "name": "floor 2",
+      "children": [
+        {
+          "name": "floor 2 help 1",
+          "children": [
+              {"name": "EricCraft"},
+              {"name": "AlbertLee"},
+              {"name": "ChrisOliver"}
+
+            ]
+          },
+          {"name": "floor 2 help 2",
+          "children": [
+              {"name": "MarkChatkha"},
+              {"name": "JPWhitaker"},
+              {"name": "AdnanWahab"}
+
+            ]
+          },
+
+            {"name": "floor 2 help 3",
+          "children": [
+              {"name": "ChristenThompson"},
+              {"name": "EmilyCoco"},
+              {"name": "ChrisOliver"}
+
+            ]
+          },
+          ]
+        },
+        {
+          "name": "floor 3",
+          "children": [
+            {
+              "name": "floor 3 help 1",
+              "children": [
+                {"name": "CharlesTai"},
+                {"name": "AlexJacobs"},
+                {"name": "AddisonLee"}
+              ]
+            },
+            {"name": "floor 3 help 2",
+          "children": [
+              {"name": "AdrianKim"},
+              {"name": "ElliottThoreby"},
+              {"name": "MeganTulac"}
+
+            ]
+           },
+            {"name": "floor 3 help 3",
+          "children": [
+              {"name": "HenryZhu"},
+              {"name": "MariusFarioletti"},
+              {"name": "ShaneKeller"}
+            ]
+        },
+          ]
+        },
+        {
+          "name": "floor 4",
+          "children": [
+            {"name": "floor 4 help 1",
+            "children": [
+              {"name": "NathanHoule"},
+              {"name": "SherahSmith"},
+              {"name": "IanLyons"}
+            ]},
+            {"name": "floor 4 help 2",
+            "children": [
+              {"name": "TimSchiller"},
+              {"name": "MarkReveley"},
+              {"name": "JunJung"}
+            ]},
+            {"name": "floor 4 help 3",
+            "children": [
+              {"name": "MarkRossetti"},
+              {"name": "IanLyons"},
+              {"name": "HenryZhu"}
+
+            ]}
+          ]
+        },
+      ]
+    };
+var makePlayer = function(target){
+
+  var player = target.append("circle").attr("r", "8").attr("class", "player");
+  player.occupies = target;
+  return player;
+
+};
+
+var movePlayerToNode = function(player, target){
+  player.attr("transform", "translate(" + target.y + ")");
+};
+
+var tryMove = function(player, target){
+  //console.log(target)
+  //console.log('target x', target.x);
+  //console.log('target y',target.y);
+  player.transition().duration(1000).attr("transform", function(d){
+    return "rotate(" + (target.x) +") translate(" + target.y + ")";
+  });
+
+  return player;
+};
+
+var mouseClickToMove = function(){
+
 }
 
-var svg = d3.select(".gameboard").append("svg:svg").attr("width", gameOptions.width).attr("height", gameOptions.height);
+
+// Start schim code
+
+var nodes = tree.nodes(jsonThing),
+    links = tree.links(nodes);
+    window.testTree = tree;
+    window.testNodes = nodes;
+    window.testLinks = links;
+
+for (var i = 0; i < nodes.length; i++) {
+  nodes[i].x = nodes[i].x + 135;
+}
+
+var link = svg.selectAll(".link")
+    .data(links)
+  .enter().append("path")
+    .attr("class", "link")
+    .attr("d", diagonal);
+window.player;
+
+var node = svg.selectAll(".node")
+  .data(nodes)
+  .enter().append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "rotate(" + (d.x) + ")translate(" + d.y + ")"; })
+    .on("click", function (obj,index){
+      //console.log('obj ', obj, 'index ', index, 'dom element ',this);
+      //console.log(obj.y);
+     tryMove(window.player, obj);
+
+    });
+
+// create player
+var g = d3.select("g");
+window.player = makePlayer(g);
 
 
-//   // DATA JOIN
-//   // Join new data with old elements, if any.
-//   var text = svg.selectAll("text")
-//       .data(data);
+node.append("circle")
+    .attr("r", 10);
 
-//   // UPDATE
-//   // Update old elements as needed.
-//   text.attr("class", "update");
+node.append("text")
+    .attr("dy", ".31em")
+    .attr("text-anchor", function(d) { return "start"; })
+    .attr("transform", function(d) { return  "rotate(45)translate(11)"; })
+    .text(function(d) { return d.name; });
 
-//   // ENTER
-//   // Create new elements as needed.
-//   text.enter().append("text")
-//       .attr("class", "enter")
-//       .attr("x", function(d, i) { return i * 32; })
-//       .attr("dy", ".35em");
+d3.select().style("height", diameter - 150 + "px");
 
-//   // ENTER + UPDATE
-//   // Appending to the enter selection expands the update selection to include
-//   // entering elements; so, operations on the update selection after appending to
-//   // the enter selection will apply to both entering and updating nodes.
-//   text.text(function(d) { return d; });
 
-//   // EXIT
-//   // Remove old elements as needed.
-//   text.exit().remove();
-// }
+// make enemies
 
-// // The initial display.
-// update(alphabet);
+window.activeRequests = {};
+window.students = (function(){
+  var nodes = d3.selectAll(".node");
+  var students =[];
+  nodes.each( function(obj){
+    if (obj.depth===3){
+      students.push(obj);
+    }
+  });
+  return students;
+})();
 
-// // Grab a random sample of letters from the alphabet, in alphabetical order.
-// setInterval(function() {
-//   update(shuffle(alphabet)
-//       .slice(0, Math.floor(Math.random() * 26))
-//       .sort());
-// }, 1500);
+window.getLinks = function(nodeName){
+  var result = [];
+  var getLink = function(node){
+    var nextNodeName;
+    d3.selectAll("path").each(function(obj){
+      window.obj = obj;
+      if (obj.target.name === nodeName) {
+        result.push([obj, this]);
+        nextNodeName = obj.source.name;
+      }
+      return nextNodeName;
+    });
 
-// // Shuffles the input array.
-// function shuffle(array) {
-//   var m = array.length, t, i;
-//   while (m) {
-//     i = Math.floor(Math.random() * m--);
-//     t = array[m], array[m] = array[i], array[i] = t;
-//   }
-//   return array;
-// }
+  };
+  var node=nodeName;
+  for (var i=0; i<3; i++){
+    node = getLink(node);
+  }
+  return result;
+};
 
+window.getLink = function(nodeName){
+  var result;
+  d3.selectAll("path").each(function(obj){
+    window.obj = obj;
+    if (obj.target.name === nodeName) {
+      result = [obj, this];
+    }
+  });
+  return result;
+};
+
+window.submitRequest = function(source){
+  if (window.activeRequests[source.name]===undefined){
+    window.activeRequests[source.name] = {
+      timeSent: Date.now(),
+      node: source
+    };
+    var d3source = d3.selectAll(".node").filter(function(thing){
+      if (thing.name === source.name){
+        return thing;
+      }
+    });
+    //console.log(d3source);
+    d3source.classed("circle", false);
+    d3source.classed("infected", true);
+  }
+};
+
+window.generateRequest = function(){
+  var leng = window.students.length;
+  var randomStudentIndex = Math.floor(d3.random.irwinHall(1)() * leng);
+  window.submitRequest(students[randomStudentIndex]);
+};
+
+window.addSpawner = function(delay){
+  setInterval(function(){
+    window.generateRequest();
+  }, (delay*d3.random.irwinHall(1)()));
+};
+
+// init
+window.addSpawner(1000 * 10);
